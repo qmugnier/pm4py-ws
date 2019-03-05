4dacf34291922d410ab062f6fac3445d07e173a2
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-05 15:04:45 +0100

    Intermediate commit

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webapp';
+
+  constructor(private router: Router) {
+    console.log("ROUTER URL");
+    console.log(router.url);
+  }
 }
