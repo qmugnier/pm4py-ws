479623de6bca66134db19f412f68528e3842c564
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-05 12:12:44 +0100

    Hea foot filt

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { ProcessModelComponent } from './process-model/process-model.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FiltersComponent } from './filters/filters.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProcessModelComponent
+    ProcessModelComponent,
+    HeaderComponent,
+    FiltersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
