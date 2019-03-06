c478ea45b0ee99a5db37d9c4c5449700ffc8b332
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-06 10:42:27 +0100

    Sexyness improved

--- webapp/src/app/process-model/process-model.component.ts
+++ webapp/src/app/process-model/process-model.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
-import { Pm4pyService } from '../pm4py-service.service';
+import {Component, OnInit} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
+import {Pm4pyService} from '../pm4py-service.service';
 import {HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-process-model',
   templateUrl: './process-model.component.html',
-  styleUrls: ['./process-model.component.scss']
+  styleUrls: ['./process-model.component.scss'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
 export class ProcessModelComponent implements OnInit {
   processModelBase64Original: string;
   processModelBase64Sanitized: SafeResourceUrl;
-  pm4pyJson : JSON;
+  pm4pyJson: JSON;
   public simplicity: number = 0.6;
-  sanitizer : DomSanitizer;
-  pm4pyService : Pm4pyService;
+  sanitizer: DomSanitizer;
+  pm4pyService: Pm4pyService;
 
   constructor(private _sanitizer: DomSanitizer, private pm4pyServ: Pm4pyService) {
     this.sanitizer = _sanitizer;
@@ -26,11 +29,27 @@ export class ProcessModelComponent implements OnInit {
     let params: HttpParams = new HttpParams();
     params.set("simplicity", this.simplicity.toString());
 
-    this.pm4pyService.getProcessSchema(params).subscribe(data => { this.pm4pyJson = data as JSON; this.processModelBase64Original = this.pm4pyJson["base64"]; this.processModelBase64Sanitized = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/svg+xml;base64,' + this.processModelBase64Original); });
+    this.pm4pyService.getProcessSchema(params).subscribe(data => {
+      this.pm4pyJson = data as JSON;
+      this.processModelBase64Original = this.pm4pyJson["base64"];
+      this.processModelBase64Sanitized = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/svg+xml;base64,' + this.processModelBase64Original);
+      this.setImageCorrectSize();
+    });
+  }
+
+  setImageCorrectSize() {
+    let targetHeight : number = (document.getElementById("container0").offsetHeight * 0.74);
+
+    document.getElementById("imageProcessModelImage").height = targetHeight;
   }
 
   ngOnInit() {
 
+  }
+
+  onResize(event){
+    event.target.innerWidth; // window width
+    this.setImageCorrectSize();
   }
 
 }
