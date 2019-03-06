18b6352ced9e9d0f1a5d4f562559bfe1e558d741
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-06 14:21:13 +0100

    Intermediate commit

--- pm4pyws/handlers/parquet/process_schema/factory.py
+++ pm4pyws/handlers/parquet/process_schema/factory.py
@@ -1,4 +1,5 @@
 from pm4pyws.handlers.parquet.process_schema.dfg_freq import get_vis as dfg_freq_vis
+from pm4pyws.handlers.parquet.process_schema.dfg_perf import get_vis as dfg_perf_vis
 from pm4pyws.handlers.parquet.process_schema.tree import get_vis as tree_vis
 from pm4pyws.handlers.parquet.process_schema.petri_freq import get_vis as petri_freq_vis
 from pm4pyws.handlers.parquet.process_schema.petri_perf import get_vis as petri_perf_vis
@@ -10,7 +11,7 @@ TREE_PERF = "tree_perf"
 PETRI_FREQ = "petri_freq"
 PETRI_PERF = "petri_perf"
 
-VERSIONS = {DFG_FREQ: dfg_freq_vis.apply, DFG_PERF: dfg_freq_vis.apply, TREE_FREQ: tree_vis.apply,
+VERSIONS = {DFG_FREQ: dfg_freq_vis.apply, DFG_PERF: dfg_perf_vis.apply, TREE_FREQ: tree_vis.apply,
             TREE_PERF: tree_vis.apply, PETRI_FREQ: petri_freq_vis.apply, PETRI_PERF: petri_perf_vis.apply}
 
 
