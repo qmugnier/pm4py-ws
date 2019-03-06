d8cc492aa16466da727c4c70dd49b90c17be0d78
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-06 09:39:31 +0100

    Intermediate commit

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProcessModelComponent } from './process-model/process-model.component';
@@ -43,6 +44,7 @@ const appRoutes: Routes = [
     HttpClientModule,
     MatSliderModule,
     MatToolbarModule,
+    FlexLayoutModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
