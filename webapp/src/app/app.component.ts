ae155b0e4d52ff3d91e076010b69e59ff9fe0556
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-10 10:51:20 +0100

    Added possibility to pass URL through query param

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webapp';
+  process_provided : boolean = false;
+  router : Router;
+  route : ActivatedRoute;
 
-  constructor(public router: Router) {
+  constructor(private _router: Router, private _route : ActivatedRoute) {
+    this.router = _router;
+    this.route = _route;
     console.log("ROUTER URL");
-    console.log(router.url);
+    console.log(_router.url);
+  }
+
+  ngOnInit() {
+    let url : string = window.location.href;
+    if (url.split("process=").length > 1) {
+      this.process_provided = true;
+      let process : string = url.split("process=")[1].split("&")[0];
+      localStorage.setItem("process", process);
+    }
+    else {
+      this.process_provided = false;
+      localStorage.setItem("process", "receipt");
+    }
   }
 }
--- webapp/src/app/pm4py-service.service.ts
+++ webapp/src/app/pm4py-service.service.ts
@@ -13,18 +13,27 @@ export class Pm4pyService {
   }
 
   getProcessSchema(parameters: HttpParams) {
+    let process = localStorage.getItem("process");
+    parameters = parameters.set("process", process);
+
     var completeUrl : string = this.webservicePath + "getProcessSchema";
 
     return this.http.get(completeUrl,{params: parameters});
   }
 
   getEventsPerTime(parameters: HttpParams) {
+    let process = localStorage.getItem("process");
+    parameters = parameters.set("process", process);
+
     var completeUrl : string = this.webservicePath + "getEventsPerTimeGraph";
 
     return this.http.get(completeUrl,{params: parameters});
   }
 
   getCaseDurationGraph(parameters: HttpParams) {
+    let process = localStorage.getItem("process");
+    parameters = parameters.set("process", process);
+
     var completeUrl : string = this.webservicePath + "getCaseDurationGraph";
 
     return this.http.get(completeUrl, {params: parameters});
