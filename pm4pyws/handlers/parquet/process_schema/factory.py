fe82ccfe1b9bb5718e8459e1cd4dd6039ebe9683
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-10 08:19:20 +0100

    Added Alpha Miner support

--- pm4pyws/handlers/parquet/process_schema/factory.py
+++ pm4pyws/handlers/parquet/process_schema/factory.py
@@ -1,8 +1,10 @@
 from pm4pyws.handlers.parquet.process_schema.dfg_freq import get_vis as dfg_freq_vis
 from pm4pyws.handlers.parquet.process_schema.dfg_perf import get_vis as dfg_perf_vis
+from pm4pyws.handlers.parquet.process_schema.inductive_freq import get_vis as inductive_freq_vis
+from pm4pyws.handlers.parquet.process_schema.inductive_perf import get_vis as inductive_perf_vis
+from pm4pyws.handlers.parquet.process_schema.alpha_freq import get_vis as alpha_freq_vis
+from pm4pyws.handlers.parquet.process_schema.alpha_performance import get_vis as alpha_perf_vis
 from pm4pyws.handlers.parquet.process_schema.tree import get_vis as tree_vis
-from pm4pyws.handlers.parquet.process_schema.inductive_freq import get_vis as petri_freq_vis
-from pm4pyws.handlers.parquet.process_schema.inductive_perf import get_vis as petri_perf_vis
 
 DFG_FREQ = "dfg_freq"
 DFG_PERF = "dfg_perf"
@@ -10,9 +12,12 @@ TREE_FREQ = "tree_freq"
 TREE_PERF = "tree_perf"
 INDUCTIVE_FREQ = "inductive_freq"
 INDUCTIVE_PERF = "inductive_perf"
+ALPHA_FREQ = "alpha_freq"
+ALPHA_PERF = "alpha_perf"
 
 VERSIONS = {DFG_FREQ: dfg_freq_vis.apply, DFG_PERF: dfg_perf_vis.apply, TREE_FREQ: tree_vis.apply,
-            TREE_PERF: tree_vis.apply, INDUCTIVE_FREQ: petri_freq_vis.apply, INDUCTIVE_PERF: petri_perf_vis.apply}
+            TREE_PERF: tree_vis.apply, INDUCTIVE_FREQ: inductive_freq_vis.apply,
+            INDUCTIVE_PERF: inductive_perf_vis.apply, ALPHA_FREQ: alpha_freq_vis.apply, ALPHA_PERF: alpha_perf_vis.apply}
 
 
 def apply(dataframe, variant=DFG_FREQ, parameters=None):
