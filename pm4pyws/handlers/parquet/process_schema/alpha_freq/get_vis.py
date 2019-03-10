791e5bc970992fd7ef5c0a3464a33df5d3468a26
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-10 08:19:20 +0100

    Added Alpha Miner support

---
+++ pm4pyws/handlers/parquet/process_schema/alpha_freq/get_vis.py
@@ -0,0 +1,25 @@
+from pm4py.algo.discovery.dfg.adapters.pandas import df_statistics
+from pm4py.algo.discovery.alpha import factory as alpha_miner
+from pm4py.algo.filtering.pandas.attributes import attributes_filter
+from pm4py.algo.filtering.pandas.auto_filter import auto_filter
+from pm4py.objects.log.util import xes
+from pm4py.visualization.common.utils import get_base64_from_gviz
+from pm4py.visualization.petrinet import factory as pn_vis_factory
+from pm4py.visualization.petrinet.util.vis_trans_shortest_paths import get_decorations_from_dfg_spaths_acticount
+from pm4py.visualization.petrinet.util.vis_trans_shortest_paths import get_shortest_paths
+
+
+def apply(dataframe, parameters=None):
+    if parameters is None:
+        parameters = {}
+    dataframe = auto_filter.apply_auto_filter(dataframe, parameters=parameters)
+    dfg = df_statistics.get_dfg_graph(dataframe)
+    activities_count = attributes_filter.get_attribute_values(dataframe, xes.DEFAULT_NAME_KEY)
+    net, im, fm = alpha_miner.apply_dfg(dfg, parameters=parameters)
+    spaths = get_shortest_paths(net)
+    aggregated_statistics = get_decorations_from_dfg_spaths_acticount(net, dfg, spaths,
+                                                                      activities_count,
+                                                                      variant="frequency")
+    gviz = pn_vis_factory.apply(net, im, fm, parameters={"format": "svg"}, variant="frequency",
+                                aggregated_statistics=aggregated_statistics)
+    return get_base64_from_gviz(gviz)
