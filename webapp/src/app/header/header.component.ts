efb3638151b937093e44264c8acca03d6ac15088
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-11 09:24:45 +0100

    Added support for loading bar

--- webapp/src/app/header/header.component.ts
+++ webapp/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -6,17 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  public processProvided : boolean;
+  public processProvided: boolean;
+  router: Router;
 
 
-  constructor() { }
+  constructor(private _route: Router) {
+    this.router = _route;
+    this.router.events.subscribe((val) => {
+      let process_name: string = localStorage.getItem("process");
+      if (process_name != null) {
+        if (this.router.url === "/logsList") {
+          this.processProvided = false;
+        }
+        else {
+          this.processProvided = true;
+        }
+      }
+      else {
+        this.processProvided = false;
+      }
+    });
+  }
 
   ngOnInit() {
     this.processProvided = false;
-    let process_name : string = localStorage.getItem("process");
+    let process_name: string = localStorage.getItem("process");
     if (process_name != null) {
       this.processProvided = true;
     }
+    else {
+      this.processProvided = false;
+    }
+  }
+
+  public resetProcessProvided(value: boolean) {
+    this.processProvided = value;
   }
 
 }
