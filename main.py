d2414b1f4582e94d6f5503a2c1b2e3e1eabb3ccd
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-11 07:37:49 +0100

    Added running-example.parquet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from pm4pyws.entrypoint import PM4PyServices
 import requests
 
+from pm4pyws.entrypoint import PM4PyServices
 
 THIS_HOST = "127.0.0.1"
 LISTENING_HOST = "0.0.0.0"
@@ -8,12 +8,14 @@ LISTENING_PORT = 5000
 
 if __name__ == "__main__":
     try:
-        r = requests.post("http://" + THIS_HOST + ":" + str(LISTENING_PORT) + "/loadLogFromPath", json={"log_name": "receipt",
-                                                                                                   "log_path": "receipt.parquet"})
-        print("service listening, told to load the log, response: "+str(r.text))
+        r = requests.post("http://" + THIS_HOST + ":" + str(LISTENING_PORT) + "/loadLogFromPath",
+                          json={"log_name": "receipt",
+                                "log_path": "receipt.parquet"})
+        print("service listening, told to load the log, response: " + str(r.text))
     except:
         print("service not listening, booting it up")
         S = PM4PyServices()
+        S.load_log("running-example", "running-example.parquet")
         S.load_log("receipt", "receipt.parquet")
-        #S.load_log("roadtraffic", "roadtraffic.parquet")
+        # S.load_log("roadtraffic", "roadtraffic.parquet")
         S.serve(host=LISTENING_HOST, port=LISTENING_PORT)
