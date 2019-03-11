9a6ccfe570fb18397ff4180889d52e9765b72caa
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-11 11:38:12 +0100

    Added comments

--- pm4pyws/entrypoint.py
+++ pm4pyws/entrypoint.py
@@ -82,6 +82,8 @@ def get_process_schema():
     variant = type_of_model + "_" + decoration
     parameters = {"decreasingFactor": simplicity}
     base64, model, format = LogsHandlers.handlers[process].get_schema(variant=variant, parameters=parameters)
+    if model is not None:
+        model = model.decode('utf-8')
     dictio = {"base64": base64.decode('utf-8'), "model": model, "format": format}
     ret = jsonify(dictio)
     return ret
--- pm4pyws/handlers/parquet/parquet.py
+++ pm4pyws/handlers/parquet/parquet.py
@@ -13,18 +13,46 @@ from pm4pyws.handlers.parquet.statistics import case_duration, events_per_time
 
 class ParquetHandler(object):
     def __init__(self):
+        """
+        Constructor (set all variables to None)
+        """
+
+        # sets the current dataframe to None
         self.dataframe = None
+        # sets the first ancestor (in the filtering chain) to None
         self.first_ancestor = None
+        # sets the last ancestor (in the filtering chain) to None
         self.last_ancestor = None
+        # sets the variant dataframe (useful in variants retrieval)
         self.variants_df = None
 
     def build_from_path(self, path, parameters=None):
+        """
+        Builds the handler from the specified path to Parquet file
+
+        Parameters
+        -------------
+        path
+            Path to the log file
+        parameters
+            Parameters of the algorithm
+        """
         if parameters is None:
             parameters = {}
         self.dataframe = parquet_importer.apply(path)
         self.build_variants_df()
 
     def build_from_csv(self, path, parameters=None):
+        """
+        Builds the handler from the specified path to CSV file
+
+        Parameters
+        -------------
+        path
+            Path to the log file
+        parameters
+            Parameters of the algorithm
+        """
         if parameters is None:
             parameters = {}
         activity_key = parameters[
@@ -33,8 +61,14 @@ class ParquetHandler(object):
             constants.PARAMETER_CONSTANT_TIMESTAMP_KEY] if constants.PARAMETER_CONSTANT_TIMESTAMP_KEY in parameters else xes.DEFAULT_TIMESTAMP_KEY
         case_id_glue = parameters[
             constants.PARAMETER_CONSTANT_CASEID_KEY] if constants.PARAMETER_CONSTANT_CASEID_KEY in parameters else CASE_CONCEPT_NAME
+        sep = parameters["sep"] if "sep" in parameters else ","
+        quotechar = parameters["quotechar"] if "quotechar" in parameters else None
 
-        self.dataframe = csv_import_adapter.import_dataframe_from_path(path)
+        if quotechar is not None:
+            self.dataframe = csv_import_adapter.import_dataframe_from_path(path, sep=sep, quotechar=quotechar)
+        else:
+            self.dataframe = csv_import_adapter.import_dataframe_from_path(path, sep=sep)
+
         if not activity_key == xes.DEFAULT_NAME_KEY:
             self.dataframe[xes.DEFAULT_NAME_KEY] = activity_key
         if not timestamp_key == xes.DEFAULT_TIMESTAMP_KEY:
@@ -44,18 +78,84 @@ class ParquetHandler(object):
         self.build_variants_df()
 
     def build_variants_df(self, parameters=None):
+        """
+        Builds the variants dataframe
+
+        Parameters
+        --------------
+        parameters
+            Possible parameters of the algorithm
+        """
         self.variants_df = case_statistics.get_variants_df(self.dataframe, parameters=parameters)
 
     def get_schema(self, variant=process_schema_factory.DFG_FREQ, parameters=None):
+        """
+        Gets the process schema in the specified variant and with the specified parameters
+
+        Parameters
+        -------------
+        variant
+            Variant of the algorithm
+        parameters
+            Parameters of the algorithm
+
+        Returns
+        ------------
+        schema
+            Process schema (in base64)
+        model
+            Model file possibly describing the process schema
+        format
+            Format of the process schema (e.g. PNML)
+        """
         return process_schema_factory.apply(self.dataframe, variant=variant, parameters=parameters)
 
     def get_case_duration_svg(self, parameters=None):
+        """
+        Gets the SVG of the case duration
+
+        Parameters
+        ------------
+        parameters
+            Parameters of the algorithm
+
+        Returns
+        -----------
+        graph
+            Case duration graph (expressed in Base 64)
+        """
         return case_duration.get_case_duration_svg(self.dataframe, parameters=parameters)
 
     def get_events_per_time_svg(self, parameters=None):
+        """
+        Gets the SVG of the events per time
+
+        Parameters
+        -------------
+        parameters
+            Parameters of the algorithm
+
+        Returns
+        -------------
+        graph
+            Events per time graph (expressed in Base 64)
+        """
         return events_per_time.get_events_per_time_svg(self.dataframe, parameters=parameters)
 
     def get_variant_statistics(self, parameters=None):
+        """
+        Gets the variants of the given log
+
+        Parameters
+        --------------
+        parameters
+            Parameters of the algorithm
+
+        Returns
+        --------------
+        variants
+            Variants of the log
+        """
         if parameters is None:
             parameters = {}
         parameters["variants_df"] = self.variants_df
@@ -63,4 +163,19 @@ class ParquetHandler(object):
         return variants.get_statistics(self.dataframe, parameters=parameters)
 
     def get_sna(self, variant="handover", parameters=None):
+        """
+        Gets a Social Network representation from a given log
+
+        Parameters
+        -------------
+        variant
+            Variant of the algorithm (metric to use)
+        parameters
+            Parameters of the algorithm (e.g. arc threshold)
+
+        Returns
+        ------------
+        sna
+            SNA representation
+        """
         return sna_obtainer.apply(self.dataframe, variant=variant, parameters=parameters)
