046ff3305e5ab661c4b35d799beb9873a24ba51b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-11 13:28:47 +0100

    Added unit tests

---
+++ tests/execute_tests.py
@@ -0,0 +1,14 @@
+import inspect
+import os
+import sys
+import unittest
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parent_dir = os.path.dirname(current_dir)
+    sys.path.insert(0, parent_dir)
+
+    from tests.test_parquet import ParquetTests
+
+    tp = ParquetTests()
+    unittest.main()
