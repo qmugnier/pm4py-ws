4f4927065ede9a3566f2e4e0fd2c4cd4fd7798ef
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-12 13:53:56 +0100

    Added filtering on maximum number of activities in the model

--- pm4pyws/handlers/xes/process_schema/dfg_perf/get_vis.py
+++ pm4pyws/handlers/xes/process_schema/dfg_perf/get_vis.py
@@ -2,7 +2,9 @@ from pm4py.algo.discovery.dfg import factory as dfg_factory
 from pm4py.visualization.dfg import factory as dfg_vis_factory
 from pm4py.visualization.common.utils import get_base64_from_gviz
 from pm4py.algo.filtering.log.auto_filter import auto_filter
+from pm4py.algo.filtering.log.attributes import attributes_filter
 
+from pm4pyws.util import constants
 
 def apply(log, parameters=None):
     """
@@ -27,6 +29,8 @@ def apply(log, parameters=None):
     if parameters is None:
         parameters = {}
 
+    log = attributes_filter.filter_log_on_max_no_activities(log, max_no_activities=constants.MAX_NO_ACTIVITIES,
+                                                            parameters=parameters)
     filtered_log = auto_filter.apply_auto_filter(log, parameters=parameters)
 
     dfg = dfg_factory.apply(filtered_log, parameters=parameters)
--- pm4pyws/handlers/xes/process_schema/tree/get_vis.py
+++ pm4pyws/handlers/xes/process_schema/tree/get_vis.py
@@ -2,7 +2,9 @@ from pm4py.algo.discovery.inductive import factory as inductive_miner
 from pm4py.visualization.common.utils import get_base64_from_gviz
 from pm4py.visualization.process_tree import factory as pt_vis_factory
 from pm4py.algo.filtering.log.auto_filter import auto_filter
+from pm4py.algo.filtering.log.attributes import attributes_filter
 
+from pm4pyws.util import constants
 
 def apply(log, parameters=None):
     """
@@ -27,6 +29,8 @@ def apply(log, parameters=None):
     if parameters is None:
         parameters = {}
 
+    log = attributes_filter.filter_log_on_max_no_activities(log, max_no_activities=constants.MAX_NO_ACTIVITIES,
+                                                            parameters=parameters)
     filtered_log = auto_filter.apply_auto_filter(log, parameters=parameters)
 
     tree = inductive_miner.apply_tree(filtered_log, parameters=parameters)
