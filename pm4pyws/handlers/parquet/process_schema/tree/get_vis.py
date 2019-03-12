965b0013f898088967fb79bd770288a4c5f1b20d
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-12 11:51:20 +0100

    Limited the number of activities in the process schema (problems with GraphViz)

--- pm4pyws/handlers/parquet/process_schema/tree/get_vis.py
+++ pm4pyws/handlers/parquet/process_schema/tree/get_vis.py
@@ -1,8 +1,11 @@
 from pm4py.algo.discovery.dfg.adapters.pandas import df_statistics
 from pm4py.algo.discovery.inductive import factory as inductive_miner
+from pm4py.algo.filtering.pandas.attributes import attributes_filter
 from pm4py.algo.filtering.pandas.auto_filter import auto_filter
 from pm4py.visualization.common.utils import get_base64_from_gviz
 from pm4py.visualization.process_tree import factory as pt_vis_factory
+
+from pm4pyws.util import constants
 
 
 def apply(dataframe, parameters=None):
@@ -27,6 +30,8 @@ def apply(dataframe, parameters=None):
     """
     if parameters is None:
         parameters = {}
+    dataframe = attributes_filter.filter_df_keeping_spno_activities(dataframe,
+                                                                    max_no_activities=constants.MAX_NO_ACTIVITIES)
     dataframe = auto_filter.apply_auto_filter(dataframe, parameters=parameters)
     dfg = df_statistics.get_dfg_graph(dataframe)
     tree = inductive_miner.apply_tree_dfg(dfg, parameters=parameters)
