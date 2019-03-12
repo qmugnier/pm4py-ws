3b4efea80c18fb68327f2a16a553360a53336a5a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-12 13:53:56 +0100

    Added filtering on maximum number of activities in the model

--- pm4pyws/handlers/xes/process_schema/alpha_freq/get_vis.py
+++ pm4pyws/handlers/xes/process_schema/alpha_freq/get_vis.py
@@ -1,8 +1,11 @@
 from pm4py.algo.discovery.alpha import factory as alpha_miner
+from pm4py.algo.filtering.log.attributes import attributes_filter
 from pm4py.algo.filtering.log.auto_filter import auto_filter
 from pm4py.objects.petri.exporter.pnml import export_petri_as_string
 from pm4py.visualization.common.utils import get_base64_from_gviz
 from pm4py.visualization.petrinet import factory as pn_vis_factory
+
+from pm4pyws.util import constants
 
 
 def apply(log, parameters=None):
@@ -28,6 +31,8 @@ def apply(log, parameters=None):
     if parameters is None:
         parameters = {}
 
+    log = attributes_filter.filter_log_on_max_no_activities(log, max_no_activities=constants.MAX_NO_ACTIVITIES,
+                                                            parameters=parameters)
     filtered_log = auto_filter.apply_auto_filter(log, parameters=parameters)
 
     net, im, fm = alpha_miner.apply(filtered_log, parameters=parameters)
