2d60aa2f7d3c2699eb9d1d4a5b9b3ebf485d6f8e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-12 14:52:44 +0100

    Refactored setup

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
+from os.path import dirname, join
+
 from setuptools import setup
 
+import pm4pyws
+
+
+def read_file(filename):
+    with open(join(dirname(__file__), filename)) as f:
+        return f.read()
+
+
 setup(
-    name='pm4py-ws',
-    version='',
+    name=pm4pyws.__name__,
+    version=pm4pyws.__version__,
+    description=pm4pyws.__doc__.strip(),
+    long_description=read_file('README.md'),
+    author=pm4pyws.__author__,
+    author_email=pm4pyws.__author_email__,
+    py_modules=[pm4pyws.__name__],
+    include_package_data=True,
     packages=['pm4pyws', 'pm4pyws.util', 'pm4pyws.handlers', 'pm4pyws.handlers.xes', 'pm4pyws.handlers.xes.sna',
               'pm4pyws.handlers.xes.cases', 'pm4pyws.handlers.xes.statistics', 'pm4pyws.handlers.xes.process_schema',
               'pm4pyws.handlers.xes.process_schema.tree', 'pm4pyws.handlers.xes.process_schema.dfg_freq',
@@ -17,10 +33,17 @@ setup(
               'pm4pyws.handlers.parquet.process_schema.inductive_freq',
               'pm4pyws.handlers.parquet.process_schema.inductive_perf',
               'pm4pyws.handlers.parquet.process_schema.alpha_performance'],
-    include_package_data=True,
-    url='',
-    license='',
-    author='PADS',
-    author_email='',
-    description=''
+    url='http://www.pm4py.org',
+    license='GPL 3.0',
+    install_requires=[
+        'pm4py',
+        'Flask',
+        'flask-cors',
+        'requests'
+    ],
+    project_urls={
+        'Documentation': 'http://pm4py.pads.rwth-aachen.de/documentation/',
+        'Source': 'https://github.com/pm4py/pm4py-source',
+        'Tracker': 'https://github.com/pm4py/pm4py-source/issues',
+    }
 )
