f29a3f8180343de34134e91592e435b06df1be9d
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-04-08 14:21:53 +0200

    Bug fix

--- pm4pyws/handlers/xes/alignments/get_align.py
+++ pm4pyws/handlers/xes/alignments/get_align.py
@@ -6,6 +6,7 @@ from pm4py.visualization.align_table import factory as align_table_factory
 from pm4py.visualization.common.utils import get_base64_from_gviz
 from pm4py.visualization.petrinet import factory as pn_vis_factory
 from pm4py.visualization.petrinet.util import alignments_decoration
+from copy import deepcopy
 
 
 def perform_alignments(log, petri_string, parameters=None):
@@ -33,13 +34,19 @@ def perform_alignments(log, petri_string, parameters=None):
 
     net, im, fm = pnml.import_petri_from_string(petri_string, parameters=parameters)
 
-    alignments = align_factory.apply(log, net, im, fm, parameters={"ret_tuple_as_trans_desc": True})
-    decorations = alignments_decoration.get_alignments_decoration(net, im, fm, aligned_traces=alignments)
+    parameters_align = deepcopy(parameters)
+    parameters_align["ret_tuple_as_trans_desc"] = True
+
+    alignments = align_factory.apply(log, net, im, fm, parameters=parameters_align)
+    decorations = alignments_decoration.get_alignments_decoration(net, im, fm, aligned_traces=alignments, parameters=parameters)
 
     gviz_on_petri = pn_vis_factory.apply(net, im, fm, aggregated_statistics=decorations, variant="alignments", parameters={"format": "svg"})
     svg_on_petri = get_base64_from_gviz(gviz_on_petri)
 
-    gviz_table = align_table_factory.apply(log, alignments, parameters={"format": "svg"})
+    parameters_table = deepcopy(parameters)
+    parameters_table["format"] = "svg"
+
+    gviz_table = align_table_factory.apply(log, alignments, parameters=parameters_table)
     svg_table = get_base64_from_gviz(gviz_table)
 
     return svg_on_petri, svg_table
