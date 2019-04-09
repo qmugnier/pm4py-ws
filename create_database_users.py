88ec2242740875ab7f6ff16fba6e8a57c0d6128b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-04-09 10:23:13 +0200

    Creation of the user database

---
+++ create_database_users.py
@@ -0,0 +1,9 @@
+import sqlite3
+conn_users = sqlite3.connect('users.db')
+curs_users = conn_users.cursor()
+
+curs_users.execute("CREATE TABLE USERS(USER_ID PRIMARY KEY, PASSWORD)")
+curs_users.execute("INSERT INTO USERS VALUES (?,?)",('admin', 'admin'))
+
+curs_users.execute("CREATE TABLE SESSIONS(SESSION_ID PRIMARY KEY, USER_ID, CREATION_DATE)")
+
