5b19e99a9e44903ce8dfa844954fb476284aa0f3
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-04-11 11:06:26 +0200

    Changes to the configuration

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   webServicePath: "http://localhost:5000/",
   enableUpload: false,
   enableDownload: true,
-  enableLogin: true,
+  enableLogin: false,
   loginTextHint: 'Username: admin Password: admin will give you full access to the system. Username: user1 Password: user1 will give you access to the running-example log along with the possibility to download the running-example. Username: user2 Password: user2 will give you access to the receipt log but without the possibility to download the log.'
 };
