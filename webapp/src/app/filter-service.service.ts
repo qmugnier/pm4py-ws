1a3a8da598036c58217eeb0a1962248e6c2432f6
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-04-11 14:19:34 +0200

    Intermediate commit

--- webapp/src/app/filter-service.service.ts
+++ webapp/src/app/filter-service.service.ts
@@ -8,8 +8,28 @@ export class FilterServiceService {
   thisProcess : string;
 
   constructor() {
+    this.filtersPerProcess = localStorage.getItem("filtersPerProcess");
+    if (this.filtersPerProcess == null) {
+      this.filtersPerProcess = new Object();
+    }
+    else {
+      this.filtersPerProcess = JSON.parse(this.filtersPerProcess);
+    }
     this.thisProcess = localStorage.getItem("process");
-    this.filtersPerProcess =  new Object();
+    if (!(this.thisProcess in this.filtersPerProcess)) {
+      this.filtersPerProcess[this.thisProcess] = [];
+    }
+  }
+
+  retrieveFiltersFromLocalStorage() {
+    this.filtersPerProcess = localStorage.getItem("filtersPerProcess");
+    if (this.filtersPerProcess == null) {
+      this.filtersPerProcess = new Object();
+    }
+    else {
+      this.filtersPerProcess = JSON.parse(this.filtersPerProcess);
+    }
+    this.thisProcess = localStorage.getItem("process");
     if (!(this.thisProcess in this.filtersPerProcess)) {
       this.filtersPerProcess[this.thisProcess] = [];
     }
@@ -18,6 +38,7 @@ export class FilterServiceService {
   addFilter(filter_type : string, filter_value : any) {
     let process : string = localStorage.getItem("process");
     this.filtersPerProcess[process].push([filter_type, filter_value]);
+    localStorage.setItem("filtersPerProcess", JSON.stringify(this.filtersPerProcess));
     console.log("SUCCESS!");
     console.log(this.filtersPerProcess);
   }
@@ -25,6 +46,9 @@ export class FilterServiceService {
   remove(filter) {
     console.log("REMOVE");
     console.log(filter);
+    let thisIndex : number = this.filtersPerProcess[this.thisProcess].indexOf(filter, 0);
+    this.filtersPerProcess[this.thisProcess].splice(thisIndex, 1);
+    localStorage.setItem("filtersPerProcess", JSON.stringify(this.filtersPerProcess));
   }
 
   getFilters() {
