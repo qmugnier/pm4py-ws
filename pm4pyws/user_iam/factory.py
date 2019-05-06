f540ba3441ef656f8cd474a709c7162654bd8a86
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-05-06 15:28:01 +0200

    Added Keycloak user management

--- pm4pyws/user_iam/factory.py
+++ pm4pyws/user_iam/factory.py
@@ -1,11 +1,13 @@
 from pm4pyws.user_iam.versions.basic_user_management import BasicUserManagement
+from pm4pyws.user_iam.versions.keycloak_user_management import KeycloakUserManagement
 
 BASIC_USER_MANAGEMENT = "basic_user_management"
+KEYCLOAK_USER_MANAGEMENT = "keycloak_user_management"
 
-VERSIONS = {BASIC_USER_MANAGEMENT: BasicUserManagement}
+VERSIONS = {BASIC_USER_MANAGEMENT: BasicUserManagement, KEYCLOAK_USER_MANAGEMENT: KeycloakUserManagement}
 
 
-def apply(ex, variant=BASIC_USER_MANAGEMENT):
+def apply(ex, variant=BASIC_USER_MANAGEMENT, parameters=None):
     """
     Gets the user manager according to the variant
 
@@ -21,4 +23,4 @@ def apply(ex, variant=BASIC_USER_MANAGEMENT):
     user_manager
         User manager
     """
-    return VERSIONS[variant](ex)
+    return VERSIONS[variant](ex, parameters=parameters)
---
+++ pm4pyws/user_iam/versions/keycloak_user_management.py
@@ -0,0 +1,97 @@
+from keycloak import KeycloakOpenID
+
+from pm4pyws.user_iam.interface.user_management import UserManagement
+
+import traceback
+
+
+class KeycloakUserManagement(UserManagement):
+    def __init__(self, ex, parameters=None):
+        if parameters is None:
+            parameters = {}
+        self.ex = ex
+        self.server_url = parameters["server_url"]
+        self.client_id = parameters["client_id"]
+        self.realm_name = parameters["realm_name"]
+        self.client_secret_key = parameters["client_secret_key"]
+
+        self.keycloak_manager = KeycloakOpenID(server_url=self.server_url, client_id=self.client_id,
+                                               realm_name=self.realm_name, client_secret_key=self.client_secret_key)
+
+        UserManagement.__init__(self, ex)
+
+    def do_login(self, user, password):
+        """
+        Logs in a user and returns a session id
+
+        Parameters
+        ------------
+        user
+            Username
+        password
+            Password
+
+        Returns
+        ------------
+        session_id
+            Session ID
+        """
+        try:
+            token = self.keycloak_manager.token(user, password)
+            return token['access_token']
+        except:
+            # traceback.print_exc()
+            pass
+        return None
+
+    def check_session_validity(self, session_id):
+        """
+        Checks the validity of a session
+
+        Parameters
+        ------------
+        session_id
+            Session ID
+
+        Returns
+        ------------
+        boolean
+            Boolean value
+        """
+        validity = False
+        try:
+            if not (str(session_id) == "null"):
+                userinfo = self.keycloak_manager.userinfo(session_id)
+                if type(userinfo["preferred_username"]) is str:
+                    if userinfo["preferred_username"]:
+                        validity = True
+        except:
+            # traceback.print_exc()
+            pass
+        return validity
+
+    def get_user_from_session(self, session_id):
+        """
+        Gets the user from the session
+
+        Parameters
+        ------------
+        session_id
+            Session ID
+
+        Returns
+        ------------
+        user
+            User ID
+        """
+        user = None
+        try:
+            if not (str(session_id) == "null"):
+                userinfo = self.keycloak_manager.userinfo(session_id)
+                if type(userinfo["preferred_username"]) is str:
+                    if userinfo["preferred_username"]:
+                        user = userinfo["preferred_username"]
+        except:
+            # traceback.print_exc()
+            pass
+        return user
