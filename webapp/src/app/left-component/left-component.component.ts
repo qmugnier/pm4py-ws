c80ff542a7fba12146dd022d86f383ba413490b7
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-05-08 10:15:16 +0200

    Intermediate commit

--- webapp/src/app/left-component/left-component.component.ts
+++ webapp/src/app/left-component/left-component.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {StartActivitiesFilterComponent} from "../start-activities-filter/start-activities-filter.component";
+import {EndActivitiesFilterComponent} from "../end-activities-filter/end-activities-filter.component";
+import {VariantsFilterComponent} from "../variants-filter/variants-filter.component";
+import {FilterServiceService} from "../filter-service.service";
+import {AttributesFilterComponent} from "../attributes-filter/attributes-filter.component";
+import {MatDialog} from '@angular/material';
+import {Pm4pyService} from "../pm4py-service.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-left-component',
@@ -6,10 +14,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-component.component.scss']
 })
 export class LeftComponentComponent implements OnInit {
+  filterService : FilterServiceService;
+  router: Router;
+  public dialog : MatDialog;
+  public processProvided: boolean;
 
-  constructor() { }
+  constructor(public _dialog: MatDialog, public _filterService : FilterServiceService, private _route: Router) {
+    this.filterService = _filterService;
+    this.filterService.retrieveFiltersFromLocalStorage();
+    this.dialog = _dialog;
+    this.router = _route;
+    this.router.events.subscribe((val) => {
+      let process_name: string = localStorage.getItem("process");
+      if (process_name != null) {
+        if (this.router.url === "/logsList" || this.router.url === "/logsList2") {
+          this.processProvided = false;
+        }
+        else if (this.router.url == "/login") {
+          this.processProvided = false;
+        }
+        else {
+          this.processProvided = true;
+        }
+      }
+      else {
+        this.processProvided = false;
+      }
+    });
+  }
 
   ngOnInit() {
+    /**
+     * Manages the initialization of the component
+     */
+    this.processProvided = false;
+    let process_name: string = localStorage.getItem("process");
+    if (process_name != null) {
+      this.processProvided = true;
+    } else {
+      this.processProvided = false;
+    }
+  }
+
+  startActivitiesFilter() {
+    this.dialog.open(StartActivitiesFilterComponent);
+  }
+
+  endActivitiesFilter() {
+    this.dialog.open(EndActivitiesFilterComponent);
+  }
+
+  variantsFilter() {
+    this.dialog.open(VariantsFilterComponent);
+  }
+
+  attributesFilter() {
+    this.dialog.open(AttributesFilterComponent);
   }
 
 }
