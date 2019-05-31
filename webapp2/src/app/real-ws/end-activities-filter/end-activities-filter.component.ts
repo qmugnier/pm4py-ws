962cd967f9db57c280ac57eb9669c29c37ae99c5
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-05-31 16:25:33 +0200

    Massive addition of new, empty, filtering components

---
+++ webapp2/src/app/real-ws/end-activities-filter/end-activities-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-end-activities-filter',
+  templateUrl: './end-activities-filter.component.html',
+  styleUrls: ['./end-activities-filter.component.scss']
+})
+export class EndActivitiesFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ webapp2/src/app/real-ws/variants-filter/variants-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-variants-filter',
+  templateUrl: './variants-filter.component.html',
+  styleUrls: ['./variants-filter.component.scss']
+})
+export class VariantsFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
