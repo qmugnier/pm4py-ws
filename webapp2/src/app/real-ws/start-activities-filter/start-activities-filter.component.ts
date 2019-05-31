04bb50cd73e91ddc631652364897349988279339
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-05-31 16:48:24 +0200

    Added start activities filter

--- webapp2/src/app/real-ws/start-activities-filter/start-activities-filter.component.ts
+++ webapp2/src/app/real-ws/start-activities-filter/start-activities-filter.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from "@angular/platform-browser";
+import {Pm4pyService} from "../../pm4py-service.service";
+import {HttpParams} from "@angular/common/http";
+import { FilterServiceService } from '../../filter-service.service';
 
 @Component({
   selector: 'app-start-activities-filter',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartActivitiesFilterComponent implements OnInit {
 
-  constructor() { }
+  sanitizer: DomSanitizer;
+  pm4pyService: Pm4pyService;
+  public startActivities : string[];
+  public selectedStartActivities : string[];
+
+  constructor(private _sanitizer: DomSanitizer, private pm4pyServ: Pm4pyService, public filterService : FilterServiceService) {
+    this.sanitizer = _sanitizer;
+    this.pm4pyService = pm4pyServ;
+    this.selectedStartActivities = [];
+    this.filterService = filterService;
+    this.getStartActivities();
+  }
 
   ngOnInit() {
+  }
+
+  getStartActivities() {
+    let httpParams : HttpParams = new HttpParams();
+    this.pm4pyService.getStartActivities(httpParams).subscribe(data => {
+      let startActivitiesJson = data as JSON;
+      this.startActivities = startActivitiesJson["startActivities"];
+      console.log(this.startActivities);
+    });
+  }
+
+  addRemoveSa(sa) {
+    if (!this.selectedStartActivities.includes(sa)) {
+      this.selectedStartActivities.push(sa);
+    }
+    else {
+      let thisIndex : number = this.selectedStartActivities.indexOf(sa, 0);
+      this.selectedStartActivities.splice(thisIndex, 1);
+    }
+  }
+
+  applyFilter() {
+    this.filterService.addFilter("start_activities", this.selectedStartActivities);
   }
 
 }
