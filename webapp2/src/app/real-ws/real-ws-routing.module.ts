ba0f1a64dd68f97975ae90e175393a831820a944
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-05-31 08:04:27 +0200

    Added authenticationService

--- webapp2/src/app/real-ws/real-ws-routing.module.ts
+++ webapp2/src/app/real-ws/real-ws-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PmodelComponent } from './pmodel/pmodel.component';
 import { CasesComponent } from './cases/cases.component';
-import { StatisticsComponent } from './statistics/statistics.component'
+import { StatisticsComponent } from './statistics/statistics.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 const routes: Routes = [
     {
@@ -29,7 +30,14 @@ const routes: Routes = [
                 data: {
                     title: 'StatisticsComponent'
                 }
-            }
+            },
+            {
+                path: 'login',
+                component: LoginComponentComponent,
+                data: {
+                    title: 'LoginComponent'
+                }
+            },
         ]
     }
 ];
