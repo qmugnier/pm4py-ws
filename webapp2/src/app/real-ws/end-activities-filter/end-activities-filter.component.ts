3061025957ee54baae1c44ff3e7cb233c3954f37
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-05-31 16:52:26 +0200

    Added end activities filter and variants filter

--- webapp2/src/app/real-ws/end-activities-filter/end-activities-filter.component.ts
+++ webapp2/src/app/real-ws/end-activities-filter/end-activities-filter.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from "@angular/platform-browser";
+import {Pm4pyService} from "../../pm4py-service.service";
+import {HttpParams} from "@angular/common/http";
+import { FilterServiceService } from '../../filter-service.service';
 
 @Component({
   selector: 'app-end-activities-filter',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EndActivitiesFilterComponent implements OnInit {
 
-  constructor() { }
+  sanitizer: DomSanitizer;
+  pm4pyService: Pm4pyService;
+  public endActivities : string[];
+  public selectedEndActivities : string[];
+
+  constructor(private _sanitizer: DomSanitizer, private pm4pyServ: Pm4pyService, public filterService : FilterServiceService) {
+    this.sanitizer = _sanitizer;
+    this.pm4pyService = pm4pyServ;
+    this.selectedEndActivities = [];
+    this.filterService = filterService;
+    this.getEndActivities();
+  }
 
   ngOnInit() {
+  }
+
+  getEndActivities() {
+    let httpParams : HttpParams = new HttpParams();
+    this.pm4pyService.getEndActivities(httpParams).subscribe(data => {
+      let endActivitiesJSON = data as JSON;
+      this.endActivities = endActivitiesJSON["endActivities"];
+      console.log(this.endActivities);
+    });
+  }
+
+  addRemoveSa(sa) {
+    if (!this.selectedEndActivities.includes(sa)) {
+      this.selectedEndActivities.push(sa);
+    }
+    else {
+      let thisIndex : number = this.selectedEndActivities.indexOf(sa, 0);
+      this.selectedEndActivities.splice(thisIndex, 1);
+    }
+  }
+
+  applyFilter() {
+    this.filterService.addFilter("end_activities", this.selectedEndActivities);
   }
 
 }
