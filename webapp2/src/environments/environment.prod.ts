d7d92756645fc7512157da0ae48af3385281f5e0
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-06-05 09:41:39 +0200

    Removed webapp2 from here

--- webapp2/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  webServicePath: "http://localhost:5000/",
-  enableUpload: true,
-  enableDownload: true,
-  enableLogin: false,
-  loginTextHint: 'Username: admin Password: admin will give you full access to the system. Username: user1 Password: user1 will give you access to the running-example log along with the possibility to download the running-example. Username: user2 Password: user2 will give you access to the receipt log but without the possibility to download the log.'
-};
