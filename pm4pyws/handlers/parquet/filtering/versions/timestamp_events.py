e754fcc20410148f2768b0b9f80234419f432c05
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-06-12 11:22:22 +0200

    Added timestamp filters versions

---
+++ pm4pyws/handlers/parquet/filtering/versions/timestamp_events.py
@@ -0,0 +1,24 @@
+from pm4py.algo.filtering.pandas.timestamp_filter import timestamp_filter
+from datetime import datetime
+
+
+def apply(dataframe, filter, parameters=None):
+    """
+    Apply a timestamp filter to the dataframe
+
+    Parameters
+    ------------
+    dataframe
+        Dataframe where the filter should be applied
+    filter
+        Filter (two timestamps separated by @@@)
+    parameters
+        Parameters of the algorithm
+    """
+    if parameters is None:
+        parameters = {}
+
+    dt1 = datetime.utcfromtimestamp(int(filter.split("@@@")[0]))
+    dt2 = datetime.utcfromtimestamp(int(filter.split("@@@")[1]))
+
+    return timestamp_filter.apply_events(dataframe, dt1, dt2, parameters=parameters)
--- pm4pyws/handlers/xes/filtering/versions/__init__.py
+++ pm4pyws/handlers/xes/filtering/versions/__init__.py
@@ -1,2 +1,3 @@
 from pm4pyws.handlers.xes.filtering.versions import start_activities, end_activities, attributes_pos_trace, \
-    attributes_neg_trace, attributes_pos_events, attributes_neg_events
+    attributes_neg_trace, attributes_pos_events, attributes_neg_events, timestamp_events, timestamp_trace_containing, \
+    timestamp_trace_intersecting
