017ce68af169845b4606de0b6f5c8d9b4fa66f8d
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-06-14 13:13:47 +0200

    Added get_graph_from_petri method

--- pm4pyws/util/get_graph.py
+++ pm4pyws/util/get_graph.py
@@ -52,5 +52,45 @@ def get_graph_from_dfg(dfg, start_activities, end_activities, parameters=None):
 
     return ["dfg", nodes, edges]
 
+
 def get_graph_from_petri(net, im, fm):
-    return []
+    """
+    Get a graph representation (nodes/edges) from a Petri net
+
+    Parameters
+    -------------
+    net
+        Petri net
+    im
+        Initial marking
+    fm
+        Final marking
+
+    Returns
+    --------------
+    graph
+        Graph representation
+    """
+    map = {}
+
+    nodes = []
+    edges = []
+
+    for place in net.places:
+        map[place.name] = len(map)
+        is_in_im = False
+        is_in_fm = False
+        if place in im:
+            is_in_im = True
+        if place in fm:
+            is_in_fm = True
+        nodes.append([place.name, place.name, "place", is_in_im, is_in_fm])
+
+    for transition in net.transitions:
+        map[transition.name] = len(map)
+        nodes.append([transition.name, transition.label, "transition", not transition.label is None])
+
+    for arc in net.arcs:
+        edges.append([map[arc.source.name], map[arc.target.name], None])
+
+    return ["petri", nodes, edges]
