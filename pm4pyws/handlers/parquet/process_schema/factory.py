c22b4a9687d988e3ee6942ea53a2fc4b04c8e758
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-06-28 16:12:04 +0200

    Fixed

--- pm4pyws/handlers/parquet/process_schema/factory.py
+++ pm4pyws/handlers/parquet/process_schema/factory.py
@@ -1,5 +1,3 @@
-from pm4pyws.handlers.parquet.process_schema.alpha_freq import get_vis as alpha_freq_vis
-from pm4pyws.handlers.parquet.process_schema.alpha_performance import get_vis as alpha_perf_vis
 from pm4pyws.handlers.parquet.process_schema.dfg_freq import get_vis as dfg_freq_vis
 from pm4pyws.handlers.parquet.process_schema.dfg_perf import get_vis as dfg_perf_vis
 from pm4pyws.handlers.parquet.process_schema.heuristics_freq import get_vis as heu_freq_vis
@@ -14,15 +12,12 @@ TREE_FREQ = "tree_freq"
 TREE_PERF = "tree_perf"
 INDUCTIVE_FREQ = "inductive_freq"
 INDUCTIVE_PERF = "inductive_perf"
-ALPHA_FREQ = "alpha_freq"
-ALPHA_PERF = "alpha_perf"
 HEURISTICS_FREQ = "heuristics_freq"
 HEURISTICS_PERF = "heuristics_perf"
 
 VERSIONS = {DFG_FREQ: dfg_freq_vis.apply, DFG_PERF: dfg_perf_vis.apply, TREE_FREQ: tree_vis.apply,
             TREE_PERF: tree_vis.apply, INDUCTIVE_FREQ: inductive_freq_vis.apply,
-            INDUCTIVE_PERF: inductive_perf_vis.apply, ALPHA_FREQ: alpha_freq_vis.apply,
-            ALPHA_PERF: alpha_perf_vis.apply, HEURISTICS_FREQ: heu_freq_vis.apply, HEURISTICS_PERF: heu_perf_vis.apply}
+            INDUCTIVE_PERF: inductive_perf_vis.apply, HEURISTICS_FREQ: heu_freq_vis.apply, HEURISTICS_PERF: heu_perf_vis.apply}
 
 
 def apply(dataframe, variant=DFG_FREQ, parameters=None):
