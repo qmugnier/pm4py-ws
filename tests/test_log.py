74dff7c58206a9469f51ec92be816e80c154b7b6
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-07 13:37:54 +0200

    Test revision

--- tests/test_log.py
+++ tests/test_log.py
@@ -4,21 +4,29 @@ import unittest
 from pm4pyws.handlers.xes.xes import XesHandler
 
 
+def do_log(path):
+    handler = XesHandler()
+    handler.build_from_path(path)
+    handler.get_schema(variant="dfg_freq")
+    handler.get_schema(variant="dfg_perf")
+    handler.get_schema(variant="inductive_freq")
+    handler.get_schema(variant="inductive_perf")
+    handler.get_schema(variant="indbpmn_freq")
+    handler.get_schema(variant="indbpmn_perf")
+    handler.get_schema(variant="heuristics_freq")
+    handler.get_schema(variant="heuristics_perf")
+    handler.get_schema(variant="tree_freq")
+    handler.get_schema(variant="tree_perf")
+    handler.get_case_duration_svg()
+    handler.get_events_per_time_svg()
+    handler.get_sna(variant="handover")
+    handler.get_sna(variant="working_together")
+    handler.get_sna(variant="subcontracting")
+    handler.get_sna(variant="jointactivities")
+    handler.get_transient(86400)
+
+
 class XesTests(unittest.TestCase):
     def test_xes(self):
-        handler = XesHandler()
-        path = os.path.join("logs//running-example.xes")
-        handler.build_from_path(path)
-        handler.get_schema(variant="dfg_freq")
-        handler.get_schema(variant="dfg_perf")
-        handler.get_schema(variant="inductive_freq")
-        handler.get_schema(variant="inductive_perf")
-        handler.get_schema(variant="tree_freq")
-        handler.get_schema(variant="tree_perf")
-        handler.get_case_duration_svg()
-        handler.get_events_per_time_svg()
-        handler.get_sna(variant="handover")
-        handler.get_sna(variant="working_together")
-        handler.get_sna(variant="subcontracting")
-        handler.get_sna(variant="jointactivities")
-        handler.get_transient(86400)
+        do_log("logs//running-example.xes")
+        do_log("logs//receipt.xes")
--- tests/test_parquet.py
+++ tests/test_parquet.py
@@ -4,23 +4,31 @@ import unittest
 from pm4pyws.handlers.parquet.parquet import ParquetHandler
 
 
+def do_log(path):
+    handler = ParquetHandler()
+    handler.build_from_path(path)
+    handler.get_schema(variant="dfg_freq")
+    handler.get_schema(variant="dfg_perf")
+    handler.get_schema(variant="inductive_freq")
+    handler.get_schema(variant="inductive_perf")
+    handler.get_schema(variant="indbpmn_freq")
+    handler.get_schema(variant="indbpmn_perf")
+    handler.get_schema(variant="heuristics_freq")
+    handler.get_schema(variant="heuristics_perf")
+    handler.get_schema(variant="tree_freq")
+    handler.get_schema(variant="tree_perf")
+    handler.get_case_duration_svg()
+    handler.get_events_per_time_svg()
+    handler.get_sna(variant="handover")
+    handler.get_sna(variant="working_together")
+    handler.get_sna(variant="subcontracting")
+    handler.get_sna(variant="jointactivities")
+    handler.get_transient(86400)
+
 class ParquetTests(unittest.TestCase):
     def test_parquets(self):
-        handler = ParquetHandler()
-        handler.build_from_path("logs//running-example.parquet")
-        handler.get_schema(variant="dfg_freq")
-        handler.get_schema(variant="dfg_perf")
-        handler.get_schema(variant="inductive_freq")
-        handler.get_schema(variant="inductive_perf")
-        handler.get_schema(variant="tree_freq")
-        handler.get_schema(variant="tree_perf")
-        handler.get_case_duration_svg()
-        handler.get_events_per_time_svg()
-        handler.get_sna(variant="handover")
-        handler.get_sna(variant="working_together")
-        handler.get_sna(variant="subcontracting")
-        handler.get_sna(variant="jointactivities")
-        handler.get_transient(86400)
+        do_log("logs//running-example.parquet")
+        do_log("logs//receipt.parquet")
 
 
 if __name__ == "__main__":
