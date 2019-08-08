bc3e282efdce00b1124f50e3675816e462171945
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-08 14:38:18 +0200

    Disabling BPMN support by default

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ try:
 except:
     pass
 
-from pm4pyws import configuration as Configuration
-
-try:
-    import pm4pybpmn
-    Configuration.enable_bpmn = True
-except:
-    Configuration.enable_bpmn = False
-
 
 from pm4pyws.entrypoint import PM4PyServices
 
