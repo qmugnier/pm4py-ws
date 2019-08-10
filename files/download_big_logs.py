2c5e69ce62193ee4b1a015e1065820dd05d96fe6
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-10 13:26:26 +0200

    Added script to download big logs and replace the database

---
+++ files/download_big_logs.py
@@ -0,0 +1,30 @@
+import sqlite3
+import os
+import shutil
+import urllib.request
+
+logs_to_download = {
+    "receipt": "http://www.alessandroberti.it/receipt.parquet",
+    "roadtraffic": "http://www.alessandroberti.it/roadtraffic.parquet"
+}
+
+shutil.rmtree("event_logs")
+os.mkdir("event_logs")
+
+conn = sqlite3.connect("databases/event_logs.db")
+curs = conn.cursor()
+
+curs.execute("DELETE FROM EVENT_LOGS")
+curs.execute("DELETE FROM USER_LOG_DOWNLOADABLE")
+curs.execute("DELETE FROM USER_LOG_REMOVAL")
+curs.execute("DELETE FROM USER_LOG_VISIBILITY")
+
+for log in logs_to_download:
+    url = logs_to_download[log]
+    urllib.request.urlretrieve(url, "event_logs/"+log+".parquet")
+
+    curs.execute("INSERT INTO EVENT_LOGS VALUES ('"+log+"','files/event_logs/"+log+".parquet',0,1,0)")
+    print("downloaded and inserted "+log)
+
+conn.commit()
+conn.close()
