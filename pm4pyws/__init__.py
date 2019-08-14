148184433ce73a7974343d36a00a35786c024f24
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-14 09:21:45 +0200

    Increased version number

--- pm4pyws/__init__.py
+++ pm4pyws/__init__.py
@@ -1,6 +1,6 @@
 from pm4pyws import entrypoint, handlers, util, log_manager, user_iam, requests_logging
 
-__version__ = '0.0.25'
+__version__ = '0.0.26'
 __doc__ = "PM4Py Web Services"
 __author__ = 'PADS'
 __author_email__ = 'pm4py@pads.rwth-aachen.de'
